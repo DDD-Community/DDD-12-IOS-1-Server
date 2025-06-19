273af2bbc63fdc968d78bd4ee6f7d38aa63852a9
seonghoo1217 <lsh6451217@gmail.com>
2025-06-19 20:06:29 +0900

    #8 chore : Beverage Img url 컬럼 추가

--- src/main/java/be/ddd/domain/entity/crawling/CafeBeverage.java
+++ src/main/java/be/ddd/domain/entity/crawling/CafeBeverage.java
@@ -1,16 +1,16 @@
 package be.ddd.domain.entity.crawling;
 
+import be.ddd.application.beverage.dto.LambdaBeverageDto;
 import be.ddd.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+// @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "cafe_beverages")
 public class CafeBeverage extends BaseTimeEntity {
 
@@ -25,8 +25,49 @@ public class CafeBeverage extends BaseTimeEntity {
     @JoinColumn(name = "cafe_store_id", nullable = false)
     private CafeStore cafeStore;
 
+    private String imgUrl;
+
     private BeverageNutrition beverageNutrition;
 
     @Enumerated(EnumType.STRING)
     private BeverageType beverageType;
+
+    public void updateFromDto(LambdaBeverageDto dto) {
+        if (dto.imgUrl() != null) {
+            this.imgUrl = dto.imgUrl();
+        }
+
+        if (dto.beverageType() != null) {
+            this.beverageType =
+                    BeverageType.valueOf(dto.beverageType().toUpperCase().replace(" ", "_"));
+        }
+
+        if (dto.beverageNutritionDto() != null) {
+            this.beverageNutrition = BeverageNutrition.from(dto.beverageNutritionDto());
+        }
+    }
+
+    public static CafeBeverage of(
+            String name,
+            CafeStore cafeStore,
+            String imgUrl,
+            BeverageNutrition beverageNutrition,
+            BeverageType beverageType) {
+        return new CafeBeverage(null, name, cafeStore, imgUrl, beverageNutrition, beverageType);
+    }
+
+    public CafeBeverage(
+            Long id,
+            String name,
+            CafeStore cafeStore,
+            String imgUrl,
+            BeverageNutrition beverageNutrition,
+            BeverageType beverageType) {
+        this.id = id;
+        this.name = name;
+        this.cafeStore = cafeStore;
+        this.imgUrl = imgUrl;
+        this.beverageNutrition = beverageNutrition;
+        this.beverageType = beverageType;
+    }
 }
---
+++ src/main/resources/db/migration/V3__add_img_url_cafe_beverages.sql
@@ -0,0 +1,2 @@
+ALTER TABLE cafe_beverages
+    ADD COLUMN img_url VARCHAR(512) NULL AFTER beverage_type;
