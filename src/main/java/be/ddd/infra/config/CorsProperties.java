62139bcb55ed1e50346d2006ca24646e8f2310a8
seonghoo1217 <lsh6451217@gmail.com>
2025-07-03 21:00:36 +0900

    #8 fix : cors config 중 headers 수정가능하도록 setter 추가

--- src/main/java/be/ddd/infra/config/CorsProperties.java
+++ src/main/java/be/ddd/infra/config/CorsProperties.java
@@ -2,12 +2,14 @@ package be.ddd.infra.config;
 
 import java.util.List;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "app.cors")
 @Getter
+@Setter
 public class CorsProperties {
     private String pathPattern;
     private List<String> allowedOrigins;
