2f3875f151dd13ea568981181ec5b78b8fd95c26
seonghoo1217 <lsh6451217@gmail.com>
2025-07-26 02:23:45 +0900

    #27 refactor :  date validator 구현시 zoned date time에서 local date time으로 로직 검증
    - 로직 범용성을 위해 시간 객체 변경

--- src/main/java/be/ddd/common/validation/NotFutureDateValidator.java
+++ src/main/java/be/ddd/common/validation/NotFutureDateValidator.java
@@ -3,12 +3,12 @@ package be.ddd.common.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
-import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
 
-public class NotFutureDateValidator implements ConstraintValidator<NotFutureDate, ZonedDateTime> {
+public class NotFutureDateValidator implements ConstraintValidator<NotFutureDate, LocalDateTime> {
 
     @Override
-    public boolean isValid(ZonedDateTime dateTime, ConstraintValidatorContext context) {
+    public boolean isValid(LocalDateTime dateTime, ConstraintValidatorContext context) {
         if (dateTime == null) {
             return true; // null 값은 @NotNull 등으로 처리
         }
