3b65f4e768e23cfd435f34fe23ddbf0212572b7e
seonghoo1217 <lsh6451217@gmail.com>
2025-07-26 01:11:30 +0900

    #27 feat : Custom 고정 시간 클래스 구현 및 사용

--- src/main/java/be/ddd/common/entity/BaseTimeEntity.java
+++ src/main/java/be/ddd/common/entity/BaseTimeEntity.java
@@ -1,9 +1,8 @@
 package be.ddd.common.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+import be.ddd.common.util.CustomClock;
+import jakarta.persistence.*;
+import java.time.ZonedDateTime;
 import lombok.Getter;
 import org.hibernate.annotations.Comment;
 import org.springframework.data.annotation.CreatedDate;
@@ -13,15 +12,29 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
 
     @CreatedDate
     @Comment("생성 일자")
     @Column(name = "CREATED_DATE", nullable = false, updatable = false)
-    private LocalDateTime createdDateTime;
+    private ZonedDateTime createdDateTime;
 
     @LastModifiedDate
     @Comment("수정 일자")
     @Column(name = "UPDATE_DATE")
-    private LocalDateTime modifiedDateTime;
+    private ZonedDateTime modifiedDateTime;
+
+    public BaseTimeEntity() {
+        createdDateTime = modifiedDateTime = CustomClock.now();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdDateTime = modifiedDateTime = CustomClock.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        modifiedDateTime = CustomClock.now();
+    }
 }
