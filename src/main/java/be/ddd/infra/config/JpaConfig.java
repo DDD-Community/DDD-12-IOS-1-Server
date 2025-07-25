d0f5572b0ed96af98261b1c98da04ed317632c07
seonghoo1217 <lsh6451217@gmail.com>
2025-07-26 01:10:05 +0900

    #27 feat : Custom 고정 시간 클래스 구현 및 사용

--- src/main/java/be/ddd/infra/config/JpaConfig.java
+++ src/main/java/be/ddd/infra/config/JpaConfig.java
@@ -1,8 +1,18 @@
 package be.ddd.infra.config;
 
+import java.time.ZonedDateTime;
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
-public class JpaConfig {}
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
+public class JpaConfig {
+
+    @Bean(name = "auditingDateTimeProvider")
+    public DateTimeProvider auditingDateTimeProvider() {
+        return () -> Optional.of(ZonedDateTime.now());
+    }
+}
